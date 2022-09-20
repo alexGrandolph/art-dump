5b8e45908957196d8c07777d93ecc8d1d10d1b4a
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-20 12:45:20 -0600

    feat: add boiler plate for header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import Header from './components/Header/Header.js';
 function App() {
   return (
     <div className="App">
-      test
+      <Header />
     </div>
   );
 }
