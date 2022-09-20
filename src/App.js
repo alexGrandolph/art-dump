4d5be68eddc27366b63c7bddde3acff73916f88a
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-20 13:54:59 -0600

    feat: add boiler plate for upload form comp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Header from './components/Header/Header.js';
+import UploadForm from './components/UploadForm/UploadForm.js';
+
 function App() {
   return (
     <div className="App">
       <Header />
+      <UploadForm />
     </div>
   );
 }
---
+++ src/components/UploadForm/UploadForm.js
@@ -0,0 +1,18 @@
+import './UploadForm.css'
+
+import React from 'react'
+
+const UploadForm = () => {
+  return (
+    <div className="upload-form-container">
+
+      <form>
+        <input type="file" />
+      </form>
+
+
+    </div>
+  )
+}
+
+export default UploadForm
