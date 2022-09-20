4ff576c602c3c369bb45267f4e54a6cfd16c325b
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-20 13:36:43 -0600

    docs: install firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyByVN8L0pCvS1eXaMJaR0CGZP0DJhLf4ag",
+  authDomain: "art-dump.firebaseapp.com",
+  projectId: "art-dump",
+  storageBucket: "art-dump.appspot.com",
+  messagingSenderId: "201371471646",
+  appId: "1:201371471646:web:71ce7900ab4551f541c30f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
