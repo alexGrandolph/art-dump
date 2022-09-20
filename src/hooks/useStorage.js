8510cc6b421d31c427bb311bedc10426049cdc5c
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-20 14:19:59 -0600

    feat: add useStorage hook under src/hooks, sets state for progress, error, and downloaded url for image from firebase

---
+++ src/hooks/useStorage.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+import { projectStorage } from '../firebase/config.js'
+
+const useStorage = (file) => {
+
+  const [progress, setProgress] = useState(0)
+  const [error, setError] = useState(null)
+  const [url, setUrl] = useState(null)
+
+  useEffect(() => {
+    //refrences
+    const storageRef = projectStorage.ref(file.name)
+
+    storageRef.put(file).on('state_changed', (snapshot) => {
+      let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      setProgress(percentage)
+    }, (err) => {
+      setError(err);
+    }, async () => {
+      const downloadedUrl = await storageRef.getDownloadUrl();
+      setUrl(downloadedUrl);
+    })
+
+  }, [file])
+
+  return { progress, url, error }
+}
