aae672118203e7817524c737701cbdeea820d590
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-20 14:06:56 -0600

    feat: add error handling / error state for if a non image file type is selected

--- src/components/UploadForm/UploadForm.js
+++ src/components/UploadForm/UploadForm.js
@@ -6,17 +6,19 @@ import React from 'react'
 
 const UploadForm = () => {
   const [file, setFile] = useState(null)
-  
+  const [error, setError] = useState(null)
+
   const allowedFileTypes = ['image/png', 'image/jpeg', 'image/jpg']
 
   const changeHandler = (event) => {
     let selected = event.target.files[0];
 
-    if (selected && types.includes(selected.type)) {
+    if (selected && allowedFileTypes.includes(selected.type)) {
       setFile(selected);
+      setError('')
     } else {
       setFile(null);
-      
+      setError('Please select an image file (.png, .jpeg, .jpg)')
     }
 
   }
@@ -26,6 +28,10 @@ const UploadForm = () => {
 
       <form>
         <input type="file" onChange={changeHandler} />
+        <div className="output">
+          { error && <div className="error">{ error }</div> }
+          { file && <div> { file.name } </div>}
+        </div>
       </form>
 
 
