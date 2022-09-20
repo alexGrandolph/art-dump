d1a12d5cd282deb36c1eeae572b7515a6ec7dd75
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-20 14:02:05 -0600

    feat: set state for File and create allowedFileTypes to check users file type for an image file type

--- src/components/UploadForm/UploadForm.js
+++ src/components/UploadForm/UploadForm.js
@@ -1,13 +1,31 @@
 import './UploadForm.css'
+import { useState } from 'react'
 
 import React from 'react'
 
+
 const UploadForm = () => {
+  const [file, setFile] = useState(null)
+  
+  const allowedFileTypes = ['image/png', 'image/jpeg', 'image/jpg']
+
+  const changeHandler = (event) => {
+    let selected = event.target.files[0];
+
+    if (selected && types.includes(selected.type)) {
+      setFile(selected);
+    } else {
+      setFile(null);
+      
+    }
+
+  }
+  
   return (
-    <div className="upload-form-container">
+    <div className="upload-form-container"> 
 
       <form>
-        <input type="file" />
+        <input type="file" onChange={changeHandler} />
       </form>
 
 
