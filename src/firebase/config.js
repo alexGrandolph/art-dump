6febd09bd141254d11e487c081970251d56f7291
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-20 13:49:47 -0600

    docs: set up firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,7 @@
-// Import the functions you need from the SDKs you need
+import * as firebase from "firebase/app";
 import { initializeApp } from "firebase/app";
+import 'firebase/storage';
+import 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,4 +16,11 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore }
