68bf275277b5c7c4b2c60aa6d5cd1a34a901d484
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-22 12:59:05 -0600

    feat: add logo to header and style it

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import './Header.css'
 import 'animate.css';
-
+import logo from '../../assets/images/logo2.png'
 import React from 'react'
 
 const Header = () => {
@@ -8,9 +8,11 @@ const Header = () => {
     <div className="title">
       <h1>alexGrandolph</h1>
       {/* <h2>..art<span className="animate__animated animate__hinge">...dump</span></h2> */}
-      <h2>..art.<span className="dump">..dump</span></h2>
+      {/* <h2>..art.<span className="dump">..dump</span></h2> */}
+      <div className="logo-container">
+        <img src={logo} className="logo" />
 
-      <div></div>
+      </div>
     </div>
   )
 }
