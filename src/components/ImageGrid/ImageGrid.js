870bd258b08e7f4787aca93170a364bedf582f77
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-22 16:26:23 -0600

    whitepsace;

--- src/components/ImageGrid/ImageGrid.js
+++ src/components/ImageGrid/ImageGrid.js
@@ -1,21 +1,21 @@
 import './ImageGrid.css';
 import useFirestore from '../../hooks/useFirestore.js';
 import { motion } from 'framer-motion'
-
   
 
 const ImageGrid = ({ setSelectedImage }) => {
 
   const { docs } = useFirestore('images');
- 
+  const constraintsRef = useRef(null);
+
   return (
     <div className="img-grid">
 
       { docs && docs.map(doc => (
         <motion.div 
           className="img-wrap" 
-          layout
           key={doc.id}
+          layout
           onClick={() => setSelectedImage(doc.url)}
           whileHover={{opacity: 1}}
         >
@@ -26,8 +26,10 @@ const ImageGrid = ({ setSelectedImage }) => {
             inital={{ opacity: 0}}  
             animate={{ opacity: 1 }}
             transition={{ delay: 5 }}
+         
           />
         </motion.div>
+      
       ))}
 
     </div>
